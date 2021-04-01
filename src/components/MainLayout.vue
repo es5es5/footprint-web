ab090b27d65440769ce4f3cd9f4f70c5f6c0f7a5
es5es5 <es5e5s@nate.com>
2021-04-01 23:50:55 +0900

    MultiSelect Setting

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
     <Header />
+    <MultiSelect
+      v-if="selectOptions"
+      :id="`paymentMeansCodeCard`"
+      v-model="multiForm.dataObject"
+      :preselectFirst="true"
+      :hideSelected="true"
+      :options="selectOptions"
+      label="title"
+      trackBy="id"
+      :multiple="isMultiple"
+      dataKey="id"
+      :dataValue.sync="multiForm.dataValue"
+      @callback="countCallback"
+    />
+    <p>multiForm.dataValue: {{ multiForm.dataValue }}</p>
+    <p>multiForm.dataObject: {{ multiForm.dataObject }}</p>
+    <p>callback: {{ callback }}</p>
   </div>
 </template>
 
@@ -11,6 +28,27 @@ export default {
   name: 'MainLayout',
   components: {
     Header
+  },
+  data () {
+    return {
+      isMultiple: false,
+      multiForm: {
+        dataValue: '2',
+      },
+      selectOptions: [{
+        title: 'Hi',
+        id: '1'
+      }, {
+        title: 'Bye',
+        id: '2'
+      }],
+      callback: 0
+    }
+  },
+  methods: {
+    countCallback () {
+      this.callback++
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+Vue.component('MultiSelect', () => import('@/../colony-vue/src/components/element/multiselect'))
+
 Vue.config.productionTip = false
 
 new Vue({
