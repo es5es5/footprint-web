c11ff9c8fc4a5c4d0f45f8ebbfee92386019f2bb
es5es5 <es5e5s@nate.com>
2021-04-08 17:30:22 +0900

    clean

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -5,8 +5,6 @@ import 'vue-burger-button/dist/vue-burger-button.css'
 import BurgerButton from 'vue-burger-button'
 import naver from 'vue-naver-maps'
 
-console.log(process.env)
-
 Vue.use(naver, {
   clientID: process.env.VUE_APP_NAVER_MAPS_CLIENT_ID,
   useGovAPI: false,
